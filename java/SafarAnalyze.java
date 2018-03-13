626c7730a4f06458cd7f2c11ca5616960b02b4d3
dafnevk <d.vankuppevelt@esciencecenter.nl>
2018-03-13 11:30:47 +0100

    Safar program now takes directory as input

--- java/SafarAnalyze.java
+++ java/SafarAnalyze.java
@@ -9,9 +9,15 @@ import safar.util.normalization.interfaces.INormalizer;
 
 import java.util.HashSet;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
+
+/**
+ * Reads all files in a directory and analyzes them with the specified analyzer
+ *
+ */
 public class SafarAnalyze {
 	
 	private static final Set<String> analyzers = new HashSet<String>(Arrays.asList(
@@ -19,25 +25,38 @@ public class SafarAnalyze {
 	));
 
 	public static void main(String[] args) {
-		String fileIn = args[0];
+		String filePathIn = args[0];
 		String filepathOut = args[1];
 		String analyzerStr = args[2];
-		
-		String basename = FilenameUtils.getBaseName(fileIn);
 		
 		if ( !analyzers.contains(analyzerStr)) {
 			System.out.println("Invalid analyzer \""+ analyzerStr +"\".");
 			System.exit(1);
 		}
 		
-		// Normalize
-		System.out.println("Normalizing...");
-		INormalizer normalizer = NormalizerFactory.getSAFARNormalizerImplementation();
-		String normalizedText = normalizer.normalize(new File(fileIn), "utf-8");
+		// List all .txt files
+		File directoryIn  = new File(filePathIn);
+		File [] inFiles = directoryIn.listFiles(new FilenameFilter() {
+		    @Override
+		    public boolean accept(File dir, String name) {
+		        return name.endsWith(".txt");
+		    }
+		});
+		for(File fileIn : inFiles) {
+			String basename = FilenameUtils.getBaseName(fileIn.getName());
+			// Normalize
+			System.out.println("Normalizing file "+basename);
+			INormalizer normalizer = NormalizerFactory.getSAFARNormalizerImplementation();
+			String normalizedText = normalizer.normalize(fileIn, "utf-8");
+			
+			IMorphologyAnalyzer analyzer = MorphologyAnalyzerFactory.getImplementation(analyzerStr);
+			String filenameOut = Paths.get(filepathOut, basename+".xml").toString();
+			analyzer.analyze(normalizedText, new File(filenameOut));
+		}
 		
-		IMorphologyAnalyzer analyzer = MorphologyAnalyzerFactory.getImplementation(analyzerStr);
-		String filenameOut = Paths.get(filepathOut, basename+".xml").toString();
-		analyzer.analyze(normalizedText, new File(filenameOut));
+		
+		
+		
 		
 		// TODO: add name of analyzer to XML (actually, SAFAR should do this)
 
