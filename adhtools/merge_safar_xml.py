b83a6c0cddf578b1ccbb19682d4315b783530346
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2018-08-01 08:45:57 +0200

    Refactor to read and write xml iteratively
    To prevent the tool using too much memory.

--- adhtools/merge_safar_xml.py
+++ adhtools/merge_safar_xml.py
@@ -1,55 +1,78 @@
 #!/usr/bin/env python
 import click
 import os
+import codecs
 
-from nlppln.utils import get_files, out_file_name, read_xml, write_xml
+from lxml import etree
 
-
-def get_total_words(soup):
-    try:
-        num = soup.morphology_analysis['total_words']
-    except TypeError:
-        num = soup.stemmer_analysis['total_words']
-    return int(num)
+from nlppln.utils import create_dirs, get_files, out_file_name
 
 
 @click.command()
 @click.argument('in_dir', type=click.Path(exists=True))
 @click.option('--out_dir', '-o', default=os.getcwd(), type=click.Path())
 def merge_safar_xml(in_dir, out_dir):
+    create_dirs(out_dir)
 
     in_files = get_files(in_dir)
 
-    if len(in_files) >= 1:
+    analysis_tag = 'morphology_analysis'
+
+    words = []
+    metadata = b'<metadata></metadata>'
+
+    if len(in_files) > 1:
+        num_words = 0
+
         fname = os.path.basename(in_files[0]).split('-')[0]
         xml_out = out_file_name(out_dir, u'{}.xml'.format(fname))
 
-        wd = read_xml(in_files[0])
+        for i, fi in enumerate(in_files):
+            if i == 0:
+                # check whether the analysis_tag should be stemmer_analysis
+                # and extract the metadata
+                context = etree.iterparse(fi, events=('end', ),
+                                          tag=('stemmer_analysis', 'metadata'))
+                for event, elem in context:
+                    if elem.tag == 'stemmer_analysis':
+                        analysis_tag = elem.tag
+                    elif elem.tag == 'metadata':
+                        metadata = etree.tostring(elem, encoding='utf-8')
 
-        total_words = get_total_words(wd)
+            # Extract the words
+            context = etree.iterparse(fi, events=('end', ), tag=('word'))
+            for event, elem in context:
+                num_words += 1
+                elem.attrib['w_id'] = str(num_words)
 
-        for fi in in_files[1:]:
-            doc = read_xml(fi)
-            tw = get_total_words(doc)
-            words = doc.find_all('word')
+                # Setting method to html (instead of xml) fixes problems
+                # with writing Arabic characters in the value attribute of
+                # the word element.
+                words.append(etree.tostring(elem, encoding='utf-8',
+                                            method='html'))
 
-            # sanity check
-            assert len(words) == tw
+                # make iteration over context fast and consume less memory
+                # https://www.ibm.com/developerworks/xml/library/x-hiperfparse
+                elem.clear()
+                while elem.getprevious() is not None:
+                    del elem.getparent()[0]
+            del context
 
-            for w in words:
-                w['w_id'] = int(w['w_id']) + total_words
-                try:
-                    wd.morphology_analysis.append(w)
-                except AttributeError:
-                    wd.stemmer_analysis.append(w)
+    # write the output
+    with codecs.open(xml_out, 'wb') as f:
+        f.write(b'<?xml version="1.0" encoding="utf-8"?>\n')
+        f.write(b'<document>\n')
 
-            total_words += tw
+        f.write(metadata)
 
-        try:
-            wd.morphology_analysis['total_words'] = total_words
-        except TypeError:
-            wd.stemmer_analysis['total_words'] = total_words
-        write_xml(wd, xml_out)
+        tag = '  <{} total_words="{}">\n'.format(analysis_tag, num_words)
+        f.write(tag.encode('utf-8'))
+
+        for w in words:
+            f.write(w)
+
+        f.write('  </{}>\n'.format(analysis_tag).encode('utf-8'))
+        f.write(b'</document>\n')
 
 
 if __name__ == '__main__':
