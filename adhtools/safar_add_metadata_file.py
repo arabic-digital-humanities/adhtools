e5d88feb49ad05fc8d94fe16d1c732d51c0dd3e9
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2018-08-20 20:22:07 +0200

    Use lxml iterparse instead of beautiful soup
    To prevent memory errors for big xml files.

--- adhtools/safar_add_metadata_file.py
+++ adhtools/safar_add_metadata_file.py
@@ -2,10 +2,10 @@
 import click
 import codecs
 import os
-import copy
-import six
-from bs4 import BeautifulSoup
-from nlppln.utils import out_file_name, get_files
+
+from lxml import etree
+
+from nlppln.utils import out_file_name, create_dirs
 
 
 @click.command()
@@ -13,31 +13,59 @@ from nlppln.utils import out_file_name, get_files
 @click.argument('in_file_meta', type=click.Path(exists=True))
 @click.option('--out_dir', '-o', default=os.getcwd(), type=click.Path())
 def safar_add_metadata(in_file, in_file_meta, out_dir):
-    doc_id = os.path.splitext(os.path.basename(in_file))[0]
-    
-    with open(in_file_meta) as fn:
-        metadata = BeautifulSoup(fn, 'xml')
+    create_dirs(out_dir)
 
-    with codecs.open(in_file, encoding='utf-8') as f:
-        soup = BeautifulSoup(f, 'xml')
+    analysis_tag = 'morphology_analysis'
+    total_words = '0'
 
-    # Make document with a single root element
-    document = BeautifulSoup('<document></document>', 'xml')
-    md = copy.copy(metadata.metadata)
-    document.document.append(md)
-    try:
-        document.document.append(soup.morphology_analysis)
-    except:
-        document.document.append(soup.stemmer_analysis)
-    
+    words = []
+    metadata = '<metadata></metadata>'
+
+    # check whether the analysis_tag should be stemmer_analysis
+    context = etree.iterparse(in_file, events=('end', ),
+                              tag=('stemmer_analysis', 'morphology_analysis'))
+    for event, elem in context:
+        if elem.tag == 'stemmer_analysis':
+            analysis_tag = elem.tag
+        total_words = elem.attrib['total_words']
+
+    # Extract the words
+    context = etree.iterparse(in_file, events=('end', ), tag=('word'))
+    for event, elem in context:
+        # Setting method to html (instead of xml) fixes problems
+        # with writing Arabic characters in the value attribute of
+        # the word element.
+        words.append(etree.tostring(elem, encoding='utf-8', method='html'))
+
+        # make iteration over context fast and consume less memory
+        # https://www.ibm.com/developerworks/xml/library/x-hiperfparse
+        elem.clear()
+        while elem.getprevious() is not None:
+            del elem.getparent()[0]
+
+    # Extract the metadata
+    with codecs.open(in_file_meta, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    if lines[0].startswith('<?'):
+        lines = lines[1:]
+    metadata = ''.join(lines)
+
+    # Write output
     xml_out = out_file_name(out_dir, in_file)
-    with codecs.open(xml_out, 'wb', encoding='utf-8') as f:
-        if six.PY2:
-            # six.u doesn't work in Python 2 with non-ascii text
-            # See https://pythonhosted.org/six/#six.u
-            f.write(unicode(document))
-        else:
-            f.write(str(document))
+    with codecs.open(xml_out, 'wb') as f:
+        f.write(b'<?xml version="1.0" encoding="utf-8"?>\n')
+        f.write(b'<document>\n')
+
+        f.write(metadata.encode('utf-8'))
+
+        tag = '  <{} total_words="{}">\n'.format(analysis_tag, total_words)
+        f.write(tag.encode('utf-8'))
+
+        for w in words:
+            f.write(w)
+
+        f.write('  </{}>\n'.format(analysis_tag).encode('utf-8'))
+        f.write(b'</document>\n')
 
 
 if __name__ == '__main__':
