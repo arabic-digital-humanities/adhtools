108879fac0716b08f26981bf7a6e412c926cc4a8
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2018-08-27 16:06:27 +0200

    Remove extraction (and writing) of metadata
    We now only use the metadata in the csv file.

--- adhtools/split_text.py
+++ adhtools/split_text.py
@@ -3,39 +3,26 @@ import codecs
 import os
 import re
 
+
 @click.command()
 @click.argument('in_file', type=click.File(encoding='utf-8'))
 @click.argument('regex')
 @click.option('--out_dir', '-o', default=os.getcwd(), type=click.Path())
 def split_text(in_file, out_dir, regex):
-
-    metadata = []
-    text = []
-    for line in in_file.readlines():
-        # TODO: metadata is sometimes inconsistent, (missing # before META,
-        # and fields not separated by :: but single :)
-        if line.startswith('#META#'):
-            metadata.append(line)
-        else:
-            text.append(line)
-       
-    metadata = u'\n'.join(metadata)
-    text = u'\n'.join(text)
-
+    text = in_file.read()
     # split based on the regex
     groups = re.split(regex, text)
-    
+
     # Write out the textfiles
     doc_name = os.path.splitext(os.path.basename(in_file.name))[0]
     for i, subtext in enumerate(groups):
         subtext = subtext.strip()
         if len(subtext) > 0:
             fname = '{}-{:05}.txt'.format(doc_name, i)
-            
-            with codecs.open(os.path.join(out_dir, fname), 'wb', encoding='utf-8') as f:
-                f.write(metadata)
-                f.write('\n')
+            fname = os.path.join(out_dir, fname)
+
+            with codecs.open(fname, 'wb', encoding='utf-8') as f:
                 f.write(subtext)
-            
+
 if __name__ == '__main__':
     split_text()
