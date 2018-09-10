6db338b94b7166f8eae2a04509707bdec8bc1f94
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2018-09-10 13:17:08 +0200

    Ignore empty lines
    Empty lines lead to empty words in the xml. We don't like that. So empty
    lines are not analyzed.

--- java/SafarAnalyze.java
+++ java/SafarAnalyze.java
@@ -31,7 +31,7 @@ import java.util.Collections;
  *
  */
 public class SafarAnalyze {
-	
+
 	private static final Set<String> analyzers = new HashSet<String>(Arrays.asList(
 			new String[] {"Alkhalil", "BAMA", "MADAMIRA"}
 	));
@@ -40,18 +40,18 @@ public class SafarAnalyze {
 		String filePathIn = args[0];
 		String filePathOut = args[1];
 		String analyzerStr = args[2];
-		
+
 		if ( !analyzers.contains(analyzerStr)) {
 			System.out.println("Invalid analyzer \""+ analyzerStr +"\".");
 			System.exit(1);
 		}
-		
+
 		// Create output directory if it doesn't exists
 		File directory = new File(filePathOut);
 	    if (!directory.exists()){
 	        directory.mkdirs();
 	    }
-		
+
 		// List all .txt files
 		File directoryIn  = new File(filePathIn);
 		File [] inFiles = directoryIn.listFiles(new FilenameFilter() {
@@ -63,20 +63,21 @@ public class SafarAnalyze {
 		for(File fileIn : inFiles) {
 			String basename = FilenameUtils.getBaseName(fileIn.getName());
 			System.out.println("Reading file "+basename);
-			
 			 try {
 				BufferedReader in = new BufferedReader(new FileReader(fileIn));
 				SAFARNormalizer normalizer = new SAFARNormalizer();
 				List<WordMorphologyAnalysis> wordMorphologyAnalysis = new LinkedList<WordMorphologyAnalysis>();
 				int i = 0;
 				for(String sentence = in.readLine(); sentence != null; sentence = in.readLine()) {
-					String normalizedText = normalizer.normalize(sentence);
-					
-					IMorphologyAnalyzer analyzer = MorphologyAnalyzerFactory.getImplementation(analyzerStr);
-					List<WordMorphologyAnalysis> wordList = analyzer.analyze(normalizedText); 
-					wordMorphologyAnalysis.addAll(wordList);
-					
-					i++;
+					if(!sentence.isEmpty()) {
+						String normalizedText = normalizer.normalize(sentence);
+
+						IMorphologyAnalyzer analyzer = MorphologyAnalyzerFactory.getImplementation(analyzerStr);
+						List<WordMorphologyAnalysis> wordList = analyzer.analyze(normalizedText);
+						wordMorphologyAnalysis.addAll(wordList);
+
+						i++;
+					}
 				}
 				System.out.println("Number of sentences: "+i);
 				in.close();
@@ -84,17 +85,17 @@ public class SafarAnalyze {
 				String filenameOut = Paths.get(filePathOut, basename+".xml").toString();
 				File fileOut = new File(filenameOut);
 				Utilities.saveAnalysisResultAsXML(wordMorphologyAnalysis, fileOut, Language.ENGLISH);
-				
+
 			 } catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			 }
 		}
-		
-		
-		
-		
-		
+
+
+
+
+
 		// TODO: add name of analyzer to XML (actually, SAFAR should do this)
 
 	}
